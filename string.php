abf96bde1ad7beb9bcad5c3f1cee06b3d5f4a020
JamanRased <41105520+JamanRased@users.noreply.github.com>
2020-04-10 23:16:45 +0600

    Add files via upload

---
+++ string.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Strings</title>
+</head>
+<body>
+	<?php
+		$X = "Hallo Jaman. How Are You";
+		//echo strlen($X);
+		//echo str_word_count($X);
+		//echo strrev($X);
+		//echo strpos($X, "Jaman");
+		echo str_replace("Hallo", "Roy", $X);
+	?>
+</body>
+</html>
